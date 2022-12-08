f88db64fafc7039302b9109fd9589f33cc5a2399
Ubuntu <ubuntu@vps-f5da0f99.vps.ovh.ca>
2022-12-08 12:17:27 +0000

    now permits adding of purchase invoices. requires the Batch Payment record to have a currency field which in our case is being retrieved from the Bank Account custom field of the same name

--- batch_payments/batch_payments/doctype/batch_payments/batch_payments.py
+++ batch_payments/batch_payments/doctype/batch_payments/batch_payments.py
@@ -3,14 +3,39 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class BatchPayments(Document):
+    pass
 
-    @frappe.whitelist()
-    def get_bills2(self):
-	frappe.msgprint('The message title 2', 'FROM SERVER PYTHON SCRIPT 2')
 
-    @frappe.whitelist()
-    def get_bills(self):
-        frappe.msgprint('get_bills2', 'FROM SERVER PYTHON SCRIPT')
+
+# get bills based on the information passed in the filter, 
+# where does the filter need to live?  On this document?
+@frappe.whitelist()
+def get_items(source_name, target_doc=None):
+    doc = get_mapped_doc(
+        "Purchase Invoice",
+        source_name,
+        {
+            "Purchase Invoice": {"doctype": "Batch Payments", "validation": {"docstatus": ["=", 1]}},
+            "Purchase Invoice Item": {
+                "doctype": "Batch Payment Items"
+            },
+        },
+        target_doc,
+    )
+    return doc
+
+
+@frappe.whitelist()
+def create_payments(self=None):
+    frappe.msgprint('create_payments', 'from batch_payments')
+@frappe.whitelist()
+def generate_file(self=None):
+    frappe.msgprint('generate_file', 'from batch_payments')
+@frappe.whitelist()
+def send_remittances(self=None):
+    frappe.msgprint('send_remittances', 'from batch_payments')
+
 
