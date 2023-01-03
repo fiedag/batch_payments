3022869764bcfdf9acc6af8a476ff5e6fd45d08c
Alex Fiedler <fiedag@users.noreply.github.com>
2023-01-03 11:36:39 +1030

    Update aba_file_formatter.py
    uncomment import frappe

--- batch_payments/batch_payments/doctype/aba_file_formatter/aba_file_formatter.py
+++ batch_payments/batch_payments/doctype/aba_file_formatter/aba_file_formatter.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Fiedler Consulting and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ABAFileFormatter(Document):
